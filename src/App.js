868bad73acb06b152a4ac9294edc7c8571025999
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2023-03-14 20:58:15 +0100

    Resolve conflict

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import { Routes,  Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 import MenuBar from "./components/MenuBar";
 import Navbar from "./components/Navbar";
-import Crash from "./pages/Crash";
-import CrashPoint from "./pages/CrashPoint"
+import CrashPoint from "./Crash/CrashPoint"
 import HomeNavBar from "./components/HomeNavBar";
 import Home from "./pages/Home";
 import axios from "axios";
@@ -112,6 +111,7 @@ function App() {
         {/* ============= Games =================== */}
 
         <Route path="/crash" element={<Crash />}></Route>
+        <Route path="CrashPoint" element={<CrashPoint />}></Route>
           {/* <Route path="BetHistory" element={<BetHistory />}></Route>
 
             <Route path="Recommend" element={<Recommend />}></Route>
